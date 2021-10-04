6b7b96c4cf8c3e9670af19aef1b23a610a56671d
Ascalonking <70878875+Ascalonking@users.noreply.github.com>
2021-10-04 11:50:05 +0500

    add chmod +x

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get --assume-yes update
